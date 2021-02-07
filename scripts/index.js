d1945fc767c78c30ab423bd71559d76ce16464d9
aymanesarrar <sarrarayman@yahoo.fr>
2021-02-07 01:26:39 +0100

    adding the author and media query

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,6 @@
 
 const placeholder = document.getElementById('quote');
+const author = document.getElementById('author');
 let random = Math.floor(Math.random() * (1643 - 0) + 0);
 fetch('https://type.fit/api/quotes')
 	.then(function (response) {
@@ -7,4 +8,9 @@ fetch('https://type.fit/api/quotes')
 	})
 	.then(function (data) {
 		placeholder.textContent = `${data[random].text}`;
+		if (!data[random].author) {
+			author.textContent = "idk man"
+		} else {
+			author.textContent = `${data[random].author}`;
+		}
 	});
